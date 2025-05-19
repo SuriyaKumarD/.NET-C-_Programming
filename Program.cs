5a98bb8a22971b67bd57f4d308e36ed6c921d205
suriya <suriyakumar.d@gmail.com>
2025-05-19 20:43:42 +0530

    Greeting App

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class GreetingsApp
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your Name:");
+        string userName = Console.ReadLine();
+
+        GreetUser(userName);
+    }
+
+    static void GreetUser(string name)
+    {
+        Console.WriteLine($"Hello,{name}! Welcome to the C#.");
+    }
+
+}
