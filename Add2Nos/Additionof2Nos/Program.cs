4fd339fc9b0e5f768827ef1c9c05f25807e1b197
Suriya_D <98840422+SuriyaKumarD@users.noreply.github.com>
2025-05-19 23:05:26 +0530

    Additon of 2nos program

---
+++ Add2Nos/Additionof2Nos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Add2Nos
+{
+
+    static void Main()
+    {
+        Console.WriteLine("Enter first number:");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the second number:");
+        int num2 = int.Parse(Console.ReadLine());
+        int result = AdditionodInput(num1, num2);
+        Console.WriteLine($"The sum of 2 numbers is: {result}");
+    }
+
+    static int AdditionodInput(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
